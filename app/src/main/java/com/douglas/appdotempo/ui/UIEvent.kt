c717eff1a403cf3d54e6ce15e18c0c521e77d98d
Douglas Stuhler <douglasstuhler518@hotmail.com>
2025-03-24 22:22:35 -0300

    Adicionando Navigation no app

---
+++ app/src/main/java/com/douglas/appdotempo/ui/UIEvent.kt
@@ -0,0 +1,4 @@
+package com.douglas.appdotempo.ui
+
+interface UIEvent {
+}
---
+++ app/src/main/java/com/douglas/appdotempo/ui/features/prevgeral/PrevGeralScreen.kt
@@ -0,0 +1,32 @@
+package com.douglas.appdotempo.ui.features.prevgeral
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import com.douglas.appdotempo.domain.previsao2
+import com.douglas.appdotempo.ui.components.labelDiaAtual
+import com.douglas.appdotempo.ui.components.resumoPrev
+import com.douglas.appdotempo.ui.theme.azul_dia
+
+@Composable
+fun prevGeral(){
+    Column (
+        modifier = Modifier
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        azul_dia,
+                        Color.White
+                    ),
+                    startY = 0f,
+                    endY = 2000f
+                )
+            )
+    ) {
+        labelDiaAtual("Uberlândia")
+        resumoPrev(previsao2)
+    }
+}
